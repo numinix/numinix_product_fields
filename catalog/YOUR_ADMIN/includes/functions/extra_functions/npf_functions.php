79e92b79e5e044bff79e756f61e9053c628c7432
ghoneim <aghoneim@ualberta.ca>
2015-05-17 11:10:15 -0600

    JCT-79 :: better fix

--- catalog/YOUR_ADMIN/includes/functions/extra_functions/npf_functions.php
+++ catalog/YOUR_ADMIN/includes/functions/extra_functions/npf_functions.php
@@ -3,19 +3,22 @@
 function dirList($directory) {
     // create an array to hold directory list
     $results = array();
-    // create a handler for the directory
-    if ($handler = opendir($directory)){
-        // keep going until all files in directory have been read
-        while ($file = readdir($handler)) {
-            // if $file isn't this directory or its parent, 
-            // add it to the results array
-            if ($file != '.' && $file != '..')
-                $results[] = $file;
+    
+    if (is_dir($directory)){
+        // create a handler for the directory
+        if ($handler = opendir($directory)){
+            // keep going until all files in directory have been read
+            while ($file = readdir($handler)) {
+                // if $file isn't this directory or its parent, 
+                // add it to the results array
+                if ($file != '.' && $file != '..')
+                    $results[] = $file;
+            }
+            asort($results);
+            // tidy up: close the handler
+            closedir($handler);
+            // done!
         }
-        asort($results);
-        // tidy up: close the handler
-        closedir($handler);
-        // done!
     }
     
     return $results;
