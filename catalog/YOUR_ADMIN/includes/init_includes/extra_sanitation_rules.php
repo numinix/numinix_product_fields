4828b228ac5fcd995975470a3142a821cd382ad4
khe817 <khe817@gmail.com>
2016-05-06 10:06:00 -0400

    PV-284 :: Fix extra sanitation init

--- catalog/YOUR_ADMIN/includes/init_includes/extra_sanitation_rules.php
+++ catalog/YOUR_ADMIN/includes/init_includes/extra_sanitation_rules.php
@@ -1,8 +1,9 @@
 <?php
 $sanitation_rules_dir = DIR_FS_ADMIN . 'includes/init_includes/extra_sanitation_rules';
+$sanitation_rules = glob($sanitation_rules_dir . "/*.php");
 
-$sanitation_rules = scandir($sanitation_rules_dir, 1);
-
-foreach ( $sanitation_rules as $sanitation_rule ) {
-	include($sanitation_rules_dir . '/' . $sanitation_rule);
+if ( !empty($sanitation_rules) ) {
+	foreach ( $sanitation_rules as $sanitation_rule ) {
+		include $sanitation_rule;
+	}
 }
