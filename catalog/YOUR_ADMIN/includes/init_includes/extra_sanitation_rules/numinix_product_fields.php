6b5bdd677f6794c9e00c76805f636a9bb3119295
khe817 <khe817@gmail.com>
2016-05-16 10:57:15 -0400

    PV-307 :: v.2.4.6 :: Update extra sanitation rule for zc 1.5.5a

--- catalog/YOUR_ADMIN/includes/init_includes/extra_sanitation_rules/numinix_product_fields.php
+++ catalog/YOUR_ADMIN/includes/init_includes/extra_sanitation_rules/numinix_product_fields.php
@@ -1,4 +1,8 @@
 <?php
 
 $group = array('products_description2', 'care_instructions', 'products_video_embed', 'products_video_embed_2');
-$sanitizer->addSanitizationGroup('PRODUCT_DESC_REGEX', $group);
+if ( method_exists($sanitizer, 'addSimpleSanitization') ) {
+	$sanitizer->addSimpleSanitization('PRODUCT_DESC_REGEX', $group);
+} elseif ( method_exists($sanitizer, 'addSanitizationGroup') ) {
+	$sanitizer->addSanitizationGroup('PRODUCT_DESC_REGEX', $group);
+}
