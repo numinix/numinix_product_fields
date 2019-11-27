335334efedd7b48dd896f2737027dd256a8f89fa
pngen <pngen93@gmail.com>
2019-11-27 11:53:25 -0800

    NX-1962 :: Issue on NPF :: Trim whitespace from data fields, Check if returned sql fields are not empty

--- catalog/includes/templates/YOUR_TEMPLATES/product_info/extra_main_template_vars/extra_main_template_vars.php
+++ catalog/includes/templates/YOUR_TEMPLATES/product_info/extra_main_template_vars/extra_main_template_vars.php
@@ -2,23 +2,25 @@
 
 global $sniffer, $db;
 //$sniffer->field_exists(TABLE_CONFIGURATION, 'configuration_tab')
-if(!empty(NUMINIX_PRODUCT_FIELDS_CATALOGUE)) {
+if (!empty(NUMINIX_PRODUCT_FIELDS_CATALOGUE)) {
 	$select = array();
 	$numinix_fields = explode(',', NUMINIX_PRODUCT_FIELDS_CATALOGUE);
 
 	foreach ($numinix_fields as $field) {
-		if($sniffer->field_exists(TABLE_PRODUCTS, $field)) {
+		$field = trim($field); // modified for NX-1962 :: Issue on NPF
+		if ($sniffer->field_exists(TABLE_PRODUCTS, $field)) {
 			$select[] = $field;
 		}
 	}
-
-	if(count($select) > 0) {
+	if (count($select) > 0) {
 		$sql_query = 'SELECT ' . implode(',', $select) . ' FROM ' . TABLE_PRODUCTS . ' WHERE products_id = :products_id';
 		$sql_query = $db->bindVars($sql_query, ':products_id', $_GET['products_id'], 'integer');
 
 		$sql = $db->Execute($sql_query);
 		foreach ($numinix_fields as $field) {
-			$numinix_fields_display[$field] = $sql->fields[$field];
+			if (!empty($sql->fields[$field])) { // bof modified for NX-1962 :: Issue on NPF
+				$numinix_fields_display[$field] = $sql->fields[$field];
+			} // eof modified for NX-1962 :: Issue on NPF
 		}
 	}
 }
