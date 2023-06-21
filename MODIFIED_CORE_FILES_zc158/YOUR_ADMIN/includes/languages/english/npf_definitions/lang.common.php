3366aee70e9606500fff422754d8bf8c7271b62c
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-06-21 00:30:03 -0400

    NX-3773 :: Numinix Product Fields - ZC -Update :: V8

---
+++ MODIFIED_CORE_FILES_zc158/YOUR_ADMIN/includes/languages/english/npf_definitions/lang.common.php
@@ -0,0 +1,9 @@
+<?php
+  $define = [ 
+  'TEXT_PRODUCTS_ASA' => 'Numinix Product Fields ',
+  'TEXT_PRODUCTS_CONDITION' => 'Condition: ',
+  'TEXT_PRODUCTS_HANDLING_FEE' => 'Handling Fee: '
+  ];
+
+  return $define;
+  // eof
