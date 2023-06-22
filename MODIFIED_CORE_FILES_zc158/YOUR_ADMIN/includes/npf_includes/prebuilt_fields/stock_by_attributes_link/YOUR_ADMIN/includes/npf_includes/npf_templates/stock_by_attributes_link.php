a6fc34fbbea2a94d909b00c89376e4c8ffad8e2f
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-06-22 18:48:08 -0400

    NX-3773 :: Numinix Product Fields - ZC -Update :: V10

--- MODIFIED_CORE_FILES_zc158/YOUR_ADMIN/includes/npf_includes/prebuilt_fields/stock_by_attributes_link/YOUR_ADMIN/includes/npf_includes/npf_templates/stock_by_attributes_link.php
+++ MODIFIED_CORE_FILES_zc158/YOUR_ADMIN/includes/npf_includes/prebuilt_fields/stock_by_attributes_link/YOUR_ADMIN/includes/npf_includes/npf_templates/stock_by_attributes_link.php
@@ -15,24 +15,26 @@ foreach($defines as $key=>$value){
   }
 }
 
-if($zc156 && defined('FILENAME_PRODUCTS_WITH_ATTRIBUTES_STOCK')){ ?>
-  <div class="form-group">
-      <?php echo zen_draw_label(TEXT_PRODUCTS_STOCK_BY_ATTRIBUTES, 'products_stock_by_attributes', 'class="col-sm-3 control-label"'); ?>
-    <div class="col-sm-9 col-md-6">
-        <?php echo '<a href="' . zen_href_link(FILENAME_PRODUCTS_WITH_ATTRIBUTES_STOCK, 'search=' . (int)$_GET['pID'] . '"', 'NONSSL') . '>Click here to go to stock by attributes</a>' ?>
-    </div>
+if(defined('FILENAME_PRODUCTS_WITH_ATTRIBUTES_STOCK')){
+  
+}else{
+if($zc156){ ?>
+<div class="form-group">
+    <?php echo zen_draw_label(TEXT_PRODUCTS_STOCK_BY_ATTRIBUTES, 'products_stock_by_attributes', 'class="col-sm-3 control-label"'); ?>
+  <div class="col-sm-9 col-md-6">
+      <?php echo '<a href="' . zen_href_link(FILENAME_PRODUCTS_WITH_ATTRIBUTES_STOCK, 'search=' . (int)$_GET['pID'] . '"', 'NONSSL') . '>Click here to go to stock by attributes</a>' ?>
   </div>
+</div>
 <?php } else { ?>
-  <tr>
-    <td colspan="2"><?php echo zen_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
-  </tr>          
-  <tr bgcolor="#DDEACC">
-    <td class="main"><?php echo TEXT_PRODUCTS_STOCK_BY_ATTRIBUTES; ?></td>
-    <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;<a href="' . zen_href_link(FILENAME_PRODUCTS_WITH_ATTRIBUTES_STOCK, 'search=' . (int)$_GET['pID'] . '"', 'NONSSL') . '>Click here to go to stock by attributes</a>'; ?></td>
-  </tr>
-  <tr>
-    <td colspan="2"><?php echo zen_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
-  </tr>
-<?php }else{
-    echo "You must install Stock By Attributes to use this feature";
-} ?>
+<tr>
+  <td colspan="2"><?php echo zen_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
+</tr>          
+<tr bgcolor="#DDEACC">
+  <td class="main"><?php echo TEXT_PRODUCTS_STOCK_BY_ATTRIBUTES; ?></td>
+  <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;<a href="' . zen_href_link(FILENAME_PRODUCTS_WITH_ATTRIBUTES_STOCK, 'search=' . (int)$_GET['pID'] . '"', 'NONSSL') . '>Click here to go to stock by attributes</a>'; ?></td>
+</tr>
+<tr>
+  <td colspan="2"><?php echo zen_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
+</tr>
+<?php }
+}?>
