1bffd36f36311f51d924503467e7727dd7d85856
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-07-24 23:49:55 -0400

    NX-3773 :: Numinix Product Fields - ZC -Update :: V19

--- ZC1.5.7/YOUR_ADMIN/includes/npf_includes/prebuilt_fields/matching_products/YOUR_ADMIN/includes/npf_includes/npf_templates/matching_products.php
+++ ZC1.5.7/YOUR_ADMIN/includes/npf_includes/prebuilt_fields/matching_products/YOUR_ADMIN/includes/npf_includes/npf_templates/matching_products.php
@@ -12,37 +12,37 @@ if($zc156){ ?>
           <div class="form-group">
               <?php echo zen_draw_label(TEXT_PRODUCTS_MATCHING_COLOR, 'matching_color', 'class="col-sm-3 control-label"'); ?>
             <div class="col-sm-9 col-md-6">
-                <?php echo zen_draw_pull_down_menu('matching_color', 'size="15"', $products_array, true, $pInfo->matching_color, true); ?>
+                <?php echo zen_draw_pull_down_menu('matching_color', $products_array, true, $pInfo->matching_color, true); ?>
             </div>
           </div>
           <div class="form-group">
               <?php echo zen_draw_label(TEXT_PRODUCTS_MATCHING_FLEECE, 'matching_fleece', 'class="col-sm-3 control-label"'); ?>
             <div class="col-sm-9 col-md-6">
-                <?php echo zen_draw_pull_down_menu('matching_fleece', 'size="15"', $products_array, true, $pInfo->matching_fleece, true); ?>
+                <?php echo zen_draw_pull_down_menu('matching_fleece', $products_array, true, $pInfo->matching_fleece, true); ?>
             </div>
           </div>
           <div class="form-group">
               <?php echo zen_draw_label(TEXT_PRODUCTS_MATCHING_TANK, 'matching_tank', 'class="col-sm-3 control-label"'); ?>
             <div class="col-sm-9 col-md-6">
-                <?php echo zen_draw_pull_down_menu('matching_tank', 'size="15"', $products_array, true, $pInfo->matching_tank, true); ?>
+                <?php echo zen_draw_pull_down_menu('matching_tank', $products_array, true, $pInfo->matching_tank, true); ?>
             </div>
           </div>
           <div class="form-group">
               <?php echo zen_draw_label(TEXT_PRODUCTS_MATCHING_TSHIRT, 'matching_tshirt', 'class="col-sm-3 control-label"'); ?>
             <div class="col-sm-9 col-md-6">
-                <?php echo zen_draw_pull_down_menu('matching_tshirt', 'size="15"', $products_array, true, $pInfo->matching_tshirt, true); ?>
+                <?php echo zen_draw_pull_down_menu('matching_tshirt', $products_array, true, $pInfo->matching_tshirt, true); ?>
             </div>
           </div>
           <div class="form-group">
               <?php echo zen_draw_label(TEXT_PRODUCTS_MATCHING_GENDER, 'matching_gender', 'class="col-sm-3 control-label"'); ?>
             <div class="col-sm-9 col-md-6">
-                <?php echo zen_draw_pull_down_menu('matching_gender', 'size="15"', $products_array, true, $pInfo->matching_gender, true); ?>
+                <?php echo zen_draw_pull_down_menu('matching_gender', $products_array, true, $pInfo->matching_gender, true); ?>
             </div>
           </div>
           <div class="form-group">
               <?php echo zen_draw_label(TEXT_PRODUCTS_MATCHING_YOUTH, 'matching_youth', 'class="col-sm-3 control-label"'); ?>
             <div class="col-sm-9 col-md-6">
-                <?php echo zen_draw_pull_down_menu('matching_youth', 'size="15"', $products_array, true, $pInfo->matching_youth, true); ?>
+                <?php echo zen_draw_pull_down_menu('matching_youth', $products_array, true, $pInfo->matching_youth, true); ?>
             </div>
           </div>
 <?php } else { ?>
@@ -51,27 +51,27 @@ if($zc156){ ?>
           </tr>          
           <tr bgcolor="#DDEACC">
             <td class="main"><?php echo TEXT_PRODUCTS_MATCHING_COLOR; ?></td>
-            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_pull_down_menu('matching_color', 'size="15"', $products_array, true, $pInfo->matching_color, true); ?></td>
+            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_pull_down_menu('matching_color', $products_array, true, $pInfo->matching_color, true); ?></td>
           </tr>
           <tr bgcolor="#DDEACC">
             <td class="main"><?php echo TEXT_PRODUCTS_MATCHING_FLEECE; ?></td>
-            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_pull_down_menu('matching_fleece', 'size="15"', $products_array, true, $pInfo->matching_fleece, true); ?></td>
+            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_pull_down_menu('matching_fleece', $products_array, true, $pInfo->matching_fleece, true); ?></td>
           </tr>
           <tr bgcolor="#DDEACC">
             <td class="main"><?php echo TEXT_PRODUCTS_MATCHING_TANK; ?></td>
-            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_pull_down_menu('matching_tank', 'size="15"', $products_array, true, $pInfo->matching_tank, true); ?></td>
+            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_pull_down_menu('matching_tank', $products_array, true, $pInfo->matching_tank, true); ?></td>
           </tr>
           <tr bgcolor="#DDEACC">
             <td class="main"><?php echo TEXT_PRODUCTS_MATCHING_TSHIRT; ?></td>
-            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_pull_down_menu('matching_tshirt', 'size="15"', $products_array, true, $pInfo->matching_tshirt, true); ?></td>
+            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_pull_down_menu('matching_tshirt', $products_array, true, $pInfo->matching_tshirt, true); ?></td>
           </tr>
           <tr bgcolor="#DDEACC">
             <td class="main"><?php echo TEXT_PRODUCTS_MATCHING_GENDER; ?></td>
-            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_pull_down_menu('matching_gender', 'size="15"', $products_array, true, $pInfo->matching_gender, true); ?></td>
+            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_pull_down_menu('matching_gender', $products_array, true, $pInfo->matching_gender, true); ?></td>
           </tr>
           <tr bgcolor="#DDEACC">
             <td class="main"><?php echo TEXT_PRODUCTS_MATCHING_YOUTH; ?></td>
-            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_pull_down_menu('matching_youth', 'size="15"', $products_array, true, $pInfo->matching_youth, true); ?></td>
+            <td class="main"><?php echo zen_draw_separator('pixel_trans.gif', '24', '15') . '&nbsp;' . zen_draw_pull_down_menu('matching_youth', $products_array, true, $pInfo->matching_youth, true); ?></td>
           </tr>                              
           <tr>
             <td colspan="2"><?php echo zen_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
